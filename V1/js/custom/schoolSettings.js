35c578cc312ddb493cf2fe5b300b0fc0a2e4d526
monil <gmonil@yahoo.co.in>
2016-05-10 21:08:02 +0530

    minor code changes

--- V1/js/custom/schoolSettings.js
+++ V1/js/custom/schoolSettings.js
@@ -14,7 +14,40 @@ app.controller("schoolSettingsController",function($scope){
 	$scope.address3="G. Gupte Road";
 	$scope.address4="Dombivli (E)";
 	$scope.address5="Maharashtra";
+
+	$scope.editData = function(){
+		$scope.showControls1 = $scope.showControls;
+		$scope.principalName1 = $scope.principalName;
+		$scope.board1 = $scope.board;
+		$scope.medium1=$scope.medium;
+		$scope.contact11=$scope.contact1;
+		$scope.contact21=$scope.contact2;
+		$scope.contact31=$scope.contact3;
+		$scope.email11=$scope.email1;
+		$scope.email21=$scope.email2;
+		$scope.address11=$scope.address1;
+		$scope.address21=$scope.address2;
+		$scope.address31=$scope.address3;
+		$scope.address41=$scope.address4;
+		$scope.address51=$scope.address5;
+		$scope.showControls=true;
+	}
+
 	$scope.saveData = function(){
+		$scope.showControls = $scope.showControls1;
+		$scope.principalName = $scope.principalName1;
+		$scope.board = $scope.board1;
+		$scope.medium=$scope.medium1;
+		$scope.contact1=$scope.contact11;
+		$scope.contact2=$scope.contact21;
+		$scope.contact3=$scope.contact31;
+		$scope.email1=$scope.email11;
+		$scope.email2=$scope.email21;
+		$scope.address1=$scope.address11;
+		$scope.address2=$scope.address21;
+		$scope.address3=$scope.address31;
+		$scope.address4=$scope.address41;
+		$scope.address5=$scope.address51;
 		$scope.showControls=false;
 	}
 });
