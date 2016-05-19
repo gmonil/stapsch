5c71aadeac681c652a95d5fd5048c324ad507d07
vipin yadav <vkyadav998@gmail.com>
2016-05-20 02:08:23 +0530

    Addition of popups

---
+++ V1/js/custom/alertPopup.js
@@ -0,0 +1,14 @@
+(function(){
+
+jQuery(document).ready(function ($) {
+
+	$('#datePicker1').datepicker({
+	    format: "dd/mm/yyyy",
+	    autoclose: true
+	});
+	$('#datePicker2').datepicker({
+	    format: "dd/mm/yyyy",
+	    autoclose: true
+	});
+});
+})();
